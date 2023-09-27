36589078701232007dbdf8b5034fd93901d072c3
Damien Duportal <damien.duportal@gmail.com>
2023-09-27 12:35:17 +0200

    feat(terraform) add a data-only module 'jenkins-infra'
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ terraform/modules/jenkins-infra/outputs.tf
@@ -0,0 +1,22 @@
+output "admin_public_ips" {
+  value = {
+    dduportal   = ["109.88.234.158"],
+    lemeurherve = ["176.185.227.180"],
+    smerle33    = ["82.64.5.129"],
+    mwaite      = ["162.142.59.220"],
+  }
+}
+
+output "outbound_ips" {
+  value = {
+    "pkg.jenkins.io"              = ["52.202.51.185"],
+    "trusted.ci.jenkins.io"       = ["104.209.128.236"],
+    "phoenix.archives.jenkins.io" = ["129.146.98.132"],
+  }
+}
+
+output "external_service_ips" {
+  value = {
+    "ftp-osl.osuosl.org" = ["140.211.166.134", "2605:bc80:3010::134"]
+  }
+}
