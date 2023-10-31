530d57aebe5241d5e96fdab11bd169585e1f6b4e
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-10-31 10:50:51 +0100

    another phone IP for lemeurherve

--- terraform/modules/jenkins-infra-shared-data/outputs.tf
+++ terraform/modules/jenkins-infra-shared-data/outputs.tf
@@ -2,7 +2,7 @@ output "admin_public_ips" {
   value = {
     dduportal           = ["86.202.255.126", "91.175.63.234"],
     lemeurherve         = ["176.185.227.180"],
-    lemeurherve_tmp_tel = ["176.145.123.59"],
+    lemeurherve_tmp_tel = ["176.145.123.59", "37.171.253.107"],
     smerle33            = ["82.64.5.129"],
     mwaite              = ["162.142.59.220"],
   }
