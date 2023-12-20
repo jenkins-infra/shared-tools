95e1b114a34698d3876e360fc8241ad4045c947a
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-12-20 15:12:33 +0100

    chore(fileshare-serviceprincipal-writer): add sensitive `password` output (#133)

--- terraform/modules/azure-jenkinsinfra-fileshare-serviceprincipal-writer/outputs.tf
+++ terraform/modules/azure-jenkinsinfra-fileshare-serviceprincipal-writer/outputs.tf
@@ -5,3 +5,8 @@ output "service_fqdn" {
 output "fileshare_serviceprincipal_writer_id" {
   value = azuread_service_principal.fileshare_serviceprincipal_writer.id
 }
+
+output "fileshare_serviceprincipal_writer_password" {
+  sensitive = true
+  value = azuread_application_password.fileshare_serviceprincipal_writer.value
+}
