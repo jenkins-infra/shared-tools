aaf51c70ab96d71f28103637250504658ae0ee65
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2024-01-25 17:28:20 +0100

    hotfix(fileshare-writer): use `azuread_service_principal_password` in output
    Fix the following error:
    
    │ Error: Unsupported attribute
    │ 
    │   on .shared-tools/terraform/modules/azure-jenkinsinfra-fileshare-serviceprincipal-writer/outputs.tf line 20, in output "fileshare_serviceprincipal_writer_sp_password":
    │   20:   value     = azuread_service_principal.fileshare_serviceprincipal_writer.value
    │ 
    │ This object has no argument, nested block, or exported attribute named "value".

--- terraform/modules/azure-jenkinsinfra-fileshare-serviceprincipal-writer/outputs.tf
+++ terraform/modules/azure-jenkinsinfra-fileshare-serviceprincipal-writer/outputs.tf
@@ -11,11 +11,15 @@ output "fileshare_serviceprincipal_writer_password" {
   value     = azuread_application_password.fileshare_serviceprincipal_writer.value
 }
 
+output "fileshare_serviceprincipal_writer_application_client_id" {
+  value = azuread_application.fileshare_serviceprincipal_writer.client_id
+}
+
 output "fileshare_serviceprincipal_writer_sp_id" {
   value = azuread_service_principal.fileshare_serviceprincipal_writer.id
 }
 
 output "fileshare_serviceprincipal_writer_sp_password" {
   sensitive = true
-  value     = azuread_service_principal.fileshare_serviceprincipal_writer.value
+  value     = azuread_service_principal_password.fileshare_serviceprincipal_writer.value
 }
