bff6ffc20b36d0ee4ce9dce68560e671e7065f2a
Damien Duportal <damien.duportal@gmail.com>
2024-04-11 14:33:11 +0200

    feat(terraform-modules/jenkins-infra-shared-data) update trusted.ci agent outbound IPs
    Follow up of https://github.com/jenkins-infra/azure-net/pull/221
    Related to https://github.com/jenkins-infra/helpdesk/issues/4029
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- terraform/modules/jenkins-infra-shared-data/outputs.tf
+++ terraform/modules/jenkins-infra-shared-data/outputs.tf
@@ -11,19 +11,25 @@ output "admin_public_ips" {
 output "outbound_ips" {
   value = {
     "pkg.jenkins.io"        = ["52.202.51.185"],
+    # Trusted controller outbound IP retrievable in https://github.com/jenkins-infra/azure-net/blob/7aa7fc5a8a39dd7bafee0e89c4fffe096692baa8/outputs.tf#L8-L10
     "trusted.ci.jenkins.io" = ["104.209.128.236"],
     "archives.jenkins.io"   = ["46.101.121.132", "2a03:b0c0:3:d0::9bc:d001"],
     "privatek8s.jenkins.io" = [
       "20.22.6.81",   # Outbound IPv4 of the cluster LB
-      "20.65.63.127", # Outbound IPv4 of the NAT gateway "privatek8s-outbound"
+      "20.65.63.127", # Outbound IPv4 of the NAT gateway - https://github.com/jenkins-infra/azure-net/blob/7aa7fc5a8a39dd7bafee0e89c4fffe096692baa8/outputs.tf#L26-L28
     ],
     "publick8s.jenkins.io" = [
       "20.22.30.74",  # Outbound IPv4 of the cluster LB
       "20.22.30.9",   # Outbound IPv4 of the cluster LB
       "20.85.71.108", # Outbound IPv4 of the cluster LB
-      "20.7.192.189", # Outbound IP of the NAT gateway "publick8s-outbound"
+      "20.7.192.189", # Outbound IP of the NAT gateway - https://github.com/jenkins-infra/azure-net/blob/7aa7fc5a8a39dd7bafee0e89c4fffe096692baa8/outputs.tf#L23-L25
     ],
-    "trusted.sponsorship.ci.jenkins.io" = ["172.177.128.34"],
+    # Trusted agents outbound IPs retrievable in https://github.com/jenkins-infra/azure-net/blob/7aa7fc5a8a39dd7bafee0e89c4fffe096692baa8/outputs.tf#L11-L13
+    "trusted.sponsorship.ci.jenkins.io" = [
+      "172.177.128.34",
+      "172.210.175.108",
+      "172.210.170.228",
+    ],
   }
 }
 
