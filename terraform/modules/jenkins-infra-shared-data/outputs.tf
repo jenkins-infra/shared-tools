34e713cfb077e5be718860156f5d68f491393a02
Damien Duportal <damien.duportal@gmail.com>
2024-04-26 10:19:50 +0200

    Update outputs.tf

--- terraform/modules/jenkins-infra-shared-data/outputs.tf
+++ terraform/modules/jenkins-infra-shared-data/outputs.tf
@@ -1,6 +1,6 @@
 output "admin_public_ips" {
   value = {
-    dduportal           = ["89.84.210.161","80.215.221.48"],
+    dduportal           = ["89.84.210.161","176.145.91.162"],
     lemeurherve         = ["176.185.227.180"],
     lemeurherve_tmp_tel = ["176.145.123.59", "37.170.86.165"],
     smerle33            = ["82.64.5.129"],
