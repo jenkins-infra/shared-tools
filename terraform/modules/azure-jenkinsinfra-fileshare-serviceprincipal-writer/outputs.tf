f14c9693936abcf8538995e43ef13abba985f174
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2024-06-26 13:03:07 +0200

    chore: add new `fileshare_serviceprincipal_writer_application_client_password` and add TODO comments to remove service principal id and password outputs (#150)

--- terraform/modules/azure-jenkinsinfra-fileshare-serviceprincipal-writer/outputs.tf
+++ terraform/modules/azure-jenkinsinfra-fileshare-serviceprincipal-writer/outputs.tf
@@ -2,10 +2,12 @@ output "service_fqdn" {
   value = var.service_fqdn
 }
 
+# TODO: to be removed, see https://github.com/jenkins-infra/helpdesk/issues/4149
 output "fileshare_serviceprincipal_writer_id" {
   value = azuread_service_principal.fileshare_serviceprincipal_writer.id
 }
 
+# TODO: to be removed & replaced by fileshare_serviceprincipal_writer_application_client_password, see https://github.com/jenkins-infra/helpdesk/issues/4149
 output "fileshare_serviceprincipal_writer_password" {
   sensitive = true
   value     = azuread_application_password.fileshare_serviceprincipal_writer.value
@@ -15,10 +17,17 @@ output "fileshare_serviceprincipal_writer_application_client_id" {
   value = azuread_application.fileshare_serviceprincipal_writer.client_id
 }
 
+output "fileshare_serviceprincipal_writer_application_client_password" {
+  sensitive = true
+  value     = azuread_application_password.fileshare_serviceprincipal_writer.value
+}
+
+# TODO: to be removed, see https://github.com/jenkins-infra/helpdesk/issues/4149
 output "fileshare_serviceprincipal_writer_sp_id" {
   value = azuread_service_principal.fileshare_serviceprincipal_writer.id
 }
 
+# TODO: to be removed, see https://github.com/jenkins-infra/helpdesk/issues/4149
 output "fileshare_serviceprincipal_writer_sp_password" {
   sensitive = true
   value     = azuread_service_principal_password.fileshare_serviceprincipal_writer.value
