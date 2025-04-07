4d27dbb98ff2af2ee07e7fef5ee89975a26a4060
Damien Duportal <damien.duportal@gmail.com>
2025-04-07 12:23:44 +0200

    feat(terraform/azure-jenkinsinfra-azurevm-agents) provide more outputs to allw tracking in other projects
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- terraform/modules/azure-jenkinsinfra-azurevm-agents/outputs.tf
+++ terraform/modules/azure-jenkinsinfra-azurevm-agents/outputs.tf
@@ -5,3 +5,19 @@ output "ephemeral_agents_nsg_name" {
 output "ephemeral_agents_resource_group_name" {
   value = azurerm_resource_group.ephemeral_agents.name
 }
+
+output "ephemeral_agents_network_rg_name" {
+  value = var.ephemeral_agents_network_rg_name
+}
+
+output "ephemeral_agents_network_name" {
+  value = var.ephemeral_agents_network_name
+}
+
+output "ephemeral_agents_subnet_name" {
+  value = var.ephemeral_agents_subnet_name
+}
+
+output "ephemeral_agents_storage_account_name" {
+  value = azurerm_storage_account.ephemeral_agents.name
+}
