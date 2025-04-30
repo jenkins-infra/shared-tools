106051ffcc3077d21f5fb4e838e34ad34274a858
Damien Duportal <damien.duportal@gmail.com>
2025-04-30 10:50:35 +0200

    cleanup(terraform/jenkins-infra-shared-data) remove `privatek8s` outbound IPs
    Ref. https://github.com/jenkins-infra/helpdesk/issues/4250#issuecomment-2839488075

--- terraform/modules/jenkins-infra-shared-data/outputs.tf
+++ terraform/modules/jenkins-infra-shared-data/outputs.tf
@@ -15,10 +15,6 @@ output "outbound_ips" {
     # Trusted controller outbound IP retrievable in https://github.com/jenkins-infra/azure-net/blob/7aa7fc5a8a39dd7bafee0e89c4fffe096692baa8/outputs.tf#L8-L10
     "trusted.ci.jenkins.io" = ["104.209.128.236"],
     "archives.jenkins.io"   = ["46.101.121.132", "2a03:b0c0:3:d0::9bc:d001"],
-    "privatek8s.jenkins.io" = [
-      "20.22.6.81",   # Outbound IPv4 of the cluster LB
-      "20.65.63.127", # Outbound IPv4 of the NAT gateway - https://github.com/jenkins-infra/azure-net/blob/7aa7fc5a8a39dd7bafee0e89c4fffe096692baa8/outputs.tf#L26-L28
-    ],
     "infracijenkinsioagents1.jenkins.io" = [
       "20.122.14.108", # Outbound IPv4 of the NAT gateway -
       "20.186.70.154", # Outbound IPv4 of the NAT gateway -
