b1f9e863258265f6b13353747c63f0d2615083b0
Damien Duportal <damien.duportal@gmail.com>
2025-07-22 18:00:14 +0200

    feat(terraform/azure-jenkinsinfra-azurevm-agents) add a new output NSG RG Name) to ease mngmt of NSGs
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- terraform/modules/azure-jenkinsinfra-azurevm-agents/outputs.tf
+++ terraform/modules/azure-jenkinsinfra-azurevm-agents/outputs.tf
@@ -1,3 +1,7 @@
+output "ephemeral_agents_nsg_rg_name" {
+  value = azurerm_network_security_group.ephemeral_agents.resource_group_name
+}
+
 output "ephemeral_agents_nsg_name" {
   value = azurerm_network_security_group.ephemeral_agents.name
 }
