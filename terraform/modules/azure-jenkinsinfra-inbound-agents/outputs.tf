70675b08f1f3f32667a698a936c782ab73c4db7c
Damien Duportal <damien.duportal@gmail.com>
2026-09-11 12:28:59 +0200

    cleanup(terraform) remove module azure-jenkinsinfra-inbound-agents (not used anymore since 2 years)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- terraform/modules/azure-jenkinsinfra-inbound-agents/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Exported to allow adding additional custom security rules
-output "inbound_agents_nsg_name" {
-  value = azurerm_network_security_group.inbound_agents.name
-}
-output "inbound_agents_nsg_rg_name" {
-  value = azurerm_network_security_group.inbound_agents.resource_group_name
-}
